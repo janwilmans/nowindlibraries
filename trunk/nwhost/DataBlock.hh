ed66e9070c3a70193ef445f962a6aad48fee20b6
janwilmans <janwilmans@631b2e32-52a8-11de-bb24-2d5e9b4af41d>
2010-03-07 13:16:34 +0000

    moved DataBlock out of NowindHost!

---
+++ trunk/nwhost/DataBlock.hh
@@ -0,0 +1,24 @@
+#ifndef DATABLOCK_HH
+#define DATABLOCK_HH
+
+#include "NowindTypes.hh"
+#include <vector>
+
+namespace nwhost {
+
+class DataBlock {
+public:
+    DataBlock(unsigned int aNumber, const std::vector <byte >& sourceData, unsigned int offset, word aTransferAddress, word size);
+
+    word number;
+    byte header;
+    word transferAddress;
+    std::vector < byte > data;
+
+    ~DataBlock();
+
+};
+
+#endif // DATABLOCK_HH
+
+} // namespace
--- trunk/nwhost/NowindHost.cc
+++ trunk/nwhost/NowindHost.cc
@@ -1,4 +1,3 @@
-
 #include "DiskHandler.hh"
 #include "SectorMedium.hh"
 #include <fstream>
@@ -1232,37 +1231,4 @@ void NowindHost::addRequest(std::vector<byte> command)
     requestQueue.push_back(command);
 }
 
-
-DataBlock::DataBlock(unsigned int aNumber, const std::vector <byte >& sourceData, unsigned int offset, word aTransferAddress, word size)
-{
-    //assert(size < 255);
-    number = aNumber;
-    transferAddress = aTransferAddress+offset+size;
-
-    bool byteInUse[256];    // byte in use map
-    for (int i=0;i<256;i++)
-    {
-        byteInUse[i] = false;
-    }
-
-    for (unsigned int i=0;i<size;i++)
-    {
-        byte currentByte = sourceData[offset+size-1-i];      // reverse the data order
-        data.push_back(currentByte);
-        byteInUse[currentByte] = true;
-    }
-
-    for (int i=0;i<256;i++)
-    {
-        if (byteInUse[i] == false)
-        {
-            // found our header (first byte not 'used' by the data)
-            header = i;
-            break;
-        }
-    }
-}
-
-
 } // namespace nowind
-
