03f553436ee2d202d8a8f6b73a7fad3fcbf99e99
janwilmans <janwilmans@631b2e32-52a8-11de-bb24-2d5e9b4af41d>
2010-05-14 14:24:56 +0000

    * fixed mixed slow and fast transfers, ghost copy works!

--- nwhost/DataBlock.cc
+++ nwhost/DataBlock.cc
@@ -4,12 +4,10 @@ namespace nwhost {
 
 DataBlock::DataBlock(unsigned int aNumber, const std::vector <byte >& sourceData, unsigned int offset, word aTransferAddress, word aSize)
 {
-    //assert(size < 255);
     number = aNumber;
     size = aSize;
+    transferAddress = aTransferAddress;
     fastTransfer = ((size & 0x7f) == 0);
-    transferAddress = aTransferAddress + size;
-    data.clear();
 
     bool byteInUse[256];    // 'byte in use' map
     for (int i=0;i<256;i++)
@@ -17,12 +15,14 @@ DataBlock::DataBlock(unsigned int aNumber, const std::vector <byte >& sourceData
         byteInUse[i] = false;
     }
     
+    data.clear();
     if (fastTransfer)
     {
+        transferAddress += size;
         for (unsigned int i=0;i<size;i++)
         {
-            byte currentByte = sourceData[offset+size-1-i];         // reverse the data order
-            data.push_back(currentByte);              
+            byte currentByte = sourceData[offset+i];         
+            data.push_front(currentByte);               // reverse the data order
             byteInUse[currentByte] = true;
         }
     }
@@ -30,8 +30,8 @@ DataBlock::DataBlock(unsigned int aNumber, const std::vector <byte >& sourceData
     {
         for (unsigned int i=0;i<size;i++)
         {
-            byte currentByte = sourceData[i];         // normal data order
-            data.push_back(currentByte);              
+            byte currentByte = sourceData[offset+i];         
+            data.push_back(currentByte);                // normal data order
             byteInUse[currentByte] = true;
         }    
     }
