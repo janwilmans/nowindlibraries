b8ee52fab122a1e48aa4e78c073f51b7baa7da55
janwilmans <janwilmans@631b2e32-52a8-11de-bb24-2d5e9b4af41d>
2010-05-16 13:12:27 +0000

    * moved all blockRead related code into BlockRead class * moved shared data into NowindHostSupport class

--- nwhost/NowindHostSupport.hh
+++ nwhost/NowindHostSupport.hh
@@ -2,6 +2,7 @@
 #define NOWNDHOSTSUPPORT_HH
 
 #include "NowindTypes.hh"
+#include <deque>
 
 namespace nwhost {
 
@@ -11,6 +12,24 @@ public:
 	NowindHostSupport();
 	virtual ~NowindHostSupport();
 	virtual void debugMessage(const char *cFormat, ...) const;
+
+	// public for usb-host implementation
+	bool isDataAvailable() const;
+
+	// read one byte of response-data from the host (msx <- pc)
+	byte read();
+
+	// like read(), but without side effects (doesn't consume the data)
+	byte peek() const;
+
+	void send(byte value);
+	void send16(word value);
+	void sendHeader();
+	void purge();
+
+private:
+	// queue
+	std::deque<byte> hostToMsxFifo;
 
 };
 
