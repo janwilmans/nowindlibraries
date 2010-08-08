18c63b9bca9e8ab941c3e9b78123d87fd896592e
aaldertdekker <aaldertdekker@631b2e32-52a8-11de-bb24-2d5e9b4af41d>
2010-08-08 21:06:36 +0000

    * added sendCommand to extbio and update nowmap.asm

--- msxsrc/msxdostools/nowmap.asm
+++ msxsrc/msxdostools/nowmap.asm
@@ -1,26 +1,27 @@
 
         output "msxdostools\nowmap.com"
-        
+
+        include "..\labels.asm"        
         include "..\asmlibs\bdos\bdos.inc"
-                
+                        
         org $0100
         
+        ld b,C_NOWMAP
+        ld de,$4e03                     ; send command
+        call EXTBIO
+
         ld a,(CMD_LENGTH)
-        ld b,a
+        ld b,0
+        ld c,a
         ld hl,CMD_LINE
         
-       
+        xor a
+        ld de,$4e04                     ; block write
+        call EXTBIO
+        
+        ; host will return message
+        ; print message here
+        ret
         
 
-
-      ret
-        
-        
-        
-helpMessage:
-        db "NOWIND partition mapper",0
-errorDriveNumber:
-        db "Invalid drive number!",0
-
-        
-        include "..\asmlibs\string\string.asm"
+;        include "..\asmlibs\string\string.asm"
