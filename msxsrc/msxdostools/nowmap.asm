5204e7e88b3bc5573c4dd0616cde0a12e25c047f
aaldertdekker <aaldertdekker@631b2e32-52a8-11de-bb24-2d5e9b4af41d>
2010-08-26 20:55:04 +0000

    * iets?

--- msxsrc/msxdostools/nowmap.asm
+++ msxsrc/msxdostools/nowmap.asm
@@ -1,11 +1,15 @@
 
         output "msxdostools\nowmap.com"
 
+        include "..\debug.asm"
         include "..\labels.asm"        
         include "..\asmlibs\bdos\bdos.inc"
                         
         org $0100
         
+        DEBUGMESSAGE "EXnowmap"
+        DEBUGDUMPREGISTERS
+        xor a                           ; first Nowind interface
         ld b,C_NOWMAP
         ld de,$4e03                     ; send command
         call EXTBIO
@@ -15,9 +19,12 @@
         ld c,a
         ld hl,CMD_LINE
         
-        xor a
+        xor a                           ; first Nowind interface
         ld de,$4e04                     ; block write
+        DEBUGDUMPREGISTERS
         call EXTBIO
+        DEBUGMESSAGE "boe"
+        DEBUGDUMPREGISTERS
         
         ; host will return message
         ; print message here
