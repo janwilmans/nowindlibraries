850c9031f5a45ad20a25e293a00934cac5b0c4ab
aaldertdekker <aaldertdekker@631b2e32-52a8-11de-bb24-2d5e9b4af41d>
2010-08-26 21:32:09 +0000

    * NOWMAP starts working!

--- emuv1/devices/SlotSelector.cpp
+++ emuv1/devices/SlotSelector.cpp
@@ -131,12 +131,12 @@ void SlotSelector::configure(unsigned int msxVersion) {
 //  usbInterface->insertDisk(0, "../disks/wb.dsk");
 //  usbInterface->insertDisk(0, "../disks/test.dsk");
 //  usbInterface->insertDisk(0, "../disks/hd5mbDOS2.dsk");
-	usbInterface->insertDisk(0, "../disks/dos1.dsk");
-//	usbInterface->insertDisk(0, "../disks/dos2.dsk");
+//	usbInterface->insertDisk(0, "../disks/dos1.dsk");
+	usbInterface->insertDisk(0, "../disks/dos2.dsk");
 
 	//usbInterface->insertDisk(0, "../disks/adnukes.dsk");
 
-//	usbInterface->insertHarddisk(0, "../disks/32mb_manuel_hd.dsk", 0);
+	usbInterface->insertHarddisk(1, "../disks/32mb_manuel_hd.dsk", 0);
 //	usbInterface->insertHarddisk(1, "../disks/32mb_manuel_hd.dsk", 1);
 //	usbInterface->insertHarddisk(2, "../disks/32mb_manuel_hd.dsk", 2);
 //  usbInterface->insertHarddisk(3, "../disks/32mb_manuel_hd.dsk", 3);
